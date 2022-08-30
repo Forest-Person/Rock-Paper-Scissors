c61b693ca0be6df7586b892f51aa281c94c74bf6
matthew <permahuman@gmail.com>
2022-08-30 08:12:39 -0700

    Create getComputerChoice funciton to return either paper scissors or rock.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 //RockPaper scissors//
-let computerChoiceArray = ['rock','paper','scissors']
 
-let computerChoice = Math.floor(Math.random()*computerChoiceArray.length)
 
-console.log(computerChoice)
+function getComputerChoice (){
+
+let computerChoiceArray = ['rock','paper','scissors'];
+let computerChoice = Math.floor(Math.random()*computerChoiceArray.length);
+
+return console.log(computerChoiceArray[computerChoice])
+}
+getComputerChoice()
+
+
 
 let humanChoice = prompt("Do you choose rock paper or scissors?")
 
+let computerScore = 0
 
-function whoWon(computerChoice[computerChoiceArray],choice)
+let humanScore = 0
 
 
-alert(`Computer chose ${computerChoiceArray[computerChoice]} and you chose ${humanChoice}`)
+/*function whoWon(computerchoice,humanchoice) {
+   
+    if (computerchoice == 'rock' && humanchoice == 'rock') {
+        return alert("It was tie")
+    
+    }else if (computerchoice == 'rock' && humanchoice == 'scissors')
+
+
+
+}
+
+
+alert(`Computer chose ${computerChoiceArray[computerChoice]} and you chose ${humanChoice}`)*/
