01e1e0098344b9e42e5cd47f5066b30d2ed40913
matthew <permahuman@gmail.com>
2022-08-30 06:58:09 -0700

    Write computer choice array, random choice, console log and prompt for human user.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 //RockPaper scissors//
+let computerChoiceArray = ['rock','paper','scissors']
 
+let computerChoice = Math.floor(Math.random()*computerChoiceArray.length)
+
+console.log(computerChoice)
+
+let choice = prompt("Do you choose rock paper or scissors?")
 
 
 
