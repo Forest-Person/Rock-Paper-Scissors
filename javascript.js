8fda99608278c12d3fe507801b0f1ff71cbe101b
matthew <permahuman@gmail.com>
2022-08-31 07:26:09 -0700

    Create score system with global human score and computerscore variables. Create if statement that tells you whether you won or not.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 //RockPaper scissors//
 
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
 
     let computerChoiceArray = ['rock', 'paper', 'scissors'];
     let computerChoice = Math.floor(Math.random() * computerChoiceArray.length);
-    console.log(computerChoice)
+    console.log(computerChoice);
     return computerChoiceArray[computerChoice];
 };
 
@@ -21,28 +23,29 @@ function getHumanChoice(){
 
 function playOneRound ( computerChoice, humanChoice) {
 
-    
+   
     
     //Deal with rock case
 
     if (computerChoice == 'rock' && humanChoice == 'scissors') {
-        
+        computerScore += 1
         return 'You lost, Rock beats Scissors!';
 
-    } else if (computerChoice == 'rock' && humanChoice == 'paper') {
-        
-        return 'You won, Paper beats Rock!';
 
+    } else if (computerChoice == 'rock' && humanChoice == 'paper') {
+        humanScore += 1
+        return 'You won, Paper beats Rock!';
+        
     } else if (computerChoice == 'rock' && humanChoice == 'rock') {
         
         return 'You tie, you both chose Rock!';
     
     } else if (computerChoice == 'paper' && humanChoice == 'rock') {
-        
+        computerScore += 1
         return 'You lost, Paper beats Rock!';
 
     } else if (computerChoice == 'paper' && humanChoice == 'scissors') {
-
+        humanScore += 1
         return 'You won, Scissors beats Paper!';
 
     } else if (computerChoice ==  'paper' && humanChoice == 'paper') {
@@ -50,26 +53,34 @@ function playOneRound ( computerChoice, humanChoice) {
         return 'You tie, you both chose paper';
 
     } else if (computerChoice == 'scissors' && humanChoice == 'paper') {
-        
+        computerScore += 1
         return 'You lost, Scissors beats Paper';
 
     } else if (computerChoice == 'scissors' && humanChoice == 'rock') {
-
+        humanScore += 1
         return 'You won, Rock beats Scissors';
 
     } else if (computerChoice == 'scissors' && humanChoice == 'scissors') {
 
         return 'You tie, you both chose scissors';
     }
-    
 }
 
+
+
 console.log(playOneRound(getComputerChoice(),getHumanChoice()));
+console.log(`Computer score is: ${computerScore} & Human Score is ${humanScore} `)
+
+if (computerScore > humanScore) {
+    console.log('COMPUTER WON!');
+} else if (computerScore < humanScore) {
+    console.log('HUMAN WON!');
+
+} else {
+
+    console.log('YOU BOTH TIED!');
+}
 
 
-
-let computerScore = 0;
-
-let humanScore = 0;
 
 
